a855e1f85b761d6894702e9b73868d37b1a3b576
Anto <antonio.colagrande.external@ynap.com>
2019-10-07 16:44:27 +0200

    emmet streams pt1

--- dom.js
+++ dom.js
@@ -1,4 +1,6 @@
-export default function html() {
+import { croak } from "./debug.js"
+
+export function html() {
 	return buildDom(
 		randomAttr(),
 		...arguments
@@ -34,3 +36,186 @@ function randomInt(){
 		Math.random() * Number.MAX_SAFE_INTEGER
 	)
 }
+
+export function emmet(strings, ...data){
+	var random = randomAttr();
+	var emmetTempString = strings.join( `a[${random}]` )
+	var stream = new TokenStream(
+		new StringStream(emmetTempString)
+	)
+	return new TagTokenStream(stream);
+}
+
+class Token {
+	constructor(value){
+		this.value = value
+	}
+}
+
+class Attribute extends Token {	
+	static fast(value){
+		var type;
+		switch(value[0]){
+			case ".":
+				type = 'class';
+				break;
+			case "#":
+				type = 'id';
+				break;
+			default:
+				croak(value);
+		}
+		value = value.replace(value[0], `${type}="`)
+		return new this(`[${value}"]`)
+	}
+}
+
+class TagName extends Token {}
+
+class Group extends Token {}
+
+class Relationship extends Token {}
+
+class Multiplier extends Token {}
+
+class TextBlock extends Token {}
+
+class TokenStream {
+	constructor(stream){
+		this.stream = stream
+	}
+
+	pick() {
+		if(!this.current){
+			this.current = this.detect(this.stream)
+		}
+		return this.current
+	}
+
+	next(){
+		var current = this.pick()
+		this.current = undefined;
+		return current
+	}
+
+	detect(){
+		var next = this.stream.pick();
+		if(!next){
+			return next;
+		}
+		switch(true){
+			case /[a-zA-Z]/.test(next):
+				return new TagName(
+					this.readStringIdentifier()
+				)
+			case /[.#]/.test(next):
+				return Attribute.fast(
+					this.stream.next() +
+					this.readStringIdentifier()
+				)
+			
+			case /[[]/.test(next):
+				return new Attribute(
+					this.readStringAttribute()
+				)
+			
+			case /[\^>+]/.test(next):
+				return new Relationship(
+					this.stream.next()
+				)
+			
+			case /[*@]/.test(next):
+				return new Multiplier(
+					this.stream.read(s => /[@*\-0-9]/.test(s))
+				)
+			
+			case /[{]/.test(next):
+				return new TextBlock(
+					this.readText()
+				)
+
+			case /[(]/.test(next):
+			case /[)]/.test(next):
+				return new Group(
+					this.stream.next()
+				)
+			default:
+				croak('Error parsing ' + next + ' ' + this.stream.current)
+		}
+	}
+
+	readText(){
+		var text = ""
+		const test = s => {
+			if((/}/).test(s)){
+				return
+			}
+			return true
+		}
+
+		while(text += this.stream.read(test)) {
+			text += this.stream.next()
+			var picked = this.stream.pick()
+			if(!picked || /[\^+)*]/.test(picked)) {
+				return text
+			} else {
+				text += this.stream.next()
+			}
+		}
+	}
+
+	readStringIdentifier(){
+		return this.stream.read(s => /[0-9a-zA-Z$-]/.test(s))
+	}
+
+	readStringAttribute(){
+		var value = this.stream.next()
+		value += this.readStringIdentifier()
+		if(this.stream.pick() == "="){
+			for(let i = 2; i > 0; i--){
+				value += this.stream.read(s => s != '"')
+				value += this.stream.next()
+			}
+		}
+		if(this.stream.pick() != ']') {
+			croak(value + this.stream.pick())
+		}
+		value += this.stream.next()
+
+		return value
+	}
+}
+
+
+class StringStream {
+	constructor(s) {
+		this.stream = s.split('')
+		this.current = 0
+	}
+
+	pick(){
+		return this.stream[0]
+	}
+
+	next(){
+		this.current++
+		return this.stream.shift();
+	}
+
+	read(f){
+		var result = "";
+		while(this.pick() && f(this.pick())){
+			result += this.next()
+		}
+		return result;
+	}
+}
+
+class TagTokenStream {
+	constructor(stream){
+		while(stream.pick()){
+			console.log(stream.next())
+		}
+	}
+}
+
